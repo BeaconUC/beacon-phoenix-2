dcf0ea131d39ef631e674af749ec37f73575d163
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2025-12-28 09:24:50 +0000

    refactor: enhance weather observation and system config schemas with validations and constraints

--- lib/beacon/stats/weather_observation.ex
+++ lib/beacon/stats/weather_observation.ex
@@ -1,19 +1,65 @@
 defmodule Beacon.Stats.WeatherObservation do
   use Beacon.Schema
   import Ecto.Changeset
+  alias Beacon.Constant
 
   @schema_prefix "stats"
 
   schema "weather_observations" do
+    field :public_id, Ecto.UUID, autogenerate: false, read_after_writes: true
 
+    field :observed_at, :utc_datetime
 
-    timestamps(type: :utc_datetime)
+    field :temperature, :decimal
+    field :feels_like, :decimal
+
+    field :humidity, :integer
+    field :atmospheric_pressure, :integer
+
+    field :wind_speed, :decimal
+    field :precipitation, :decimal
+
+    field :condition_main, :string
+    field :condition_description, :string
+
+    belongs_to :city, Beacon.Geo.City
+
+    timestamps()
   end
 
-  @doc false
+  @required_fields [
+    :public_id,
+    :city_id,
+    :observed_at,
+    :temperature
+  ]
+  @optional_fields [
+    :feels_like,
+    :humidity,
+    :atmospheric_pressure,
+    :wind_speed,
+    :precipitation,
+    :condition_main,
+    :condition_description
+  ]
   def changeset(weather_observation, attrs) do
     weather_observation
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> validate_number(:humidity,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 100
+    )
+    |> validate_number(:atmospheric_pressure, greater_than: 0)
+    |> validate_number(:wind_speed, greater_than_or_equal_to: 0.0)
+    |> validate_number(:precipitation, greater_than_or_equal_to: 0.0)
+    |> validate_length(:condition_main, max: Constant.varchar_max_length())
+    |> validate_length(:condition_description, max: Constant.varchar_max_length())
+    |> foreign_key_constraint(:city_id)
+    |> unique_constraint(
+      [:city_id, :observed_at],
+      name: :weather_observations_city_id_observed_at_idx
+    )
+    |> unique_constraint(:public_id, name: :weather_observations_public_id_idx)
   end
 end
--- lib/beacon/sys/system_config.ex
+++ lib/beacon/sys/system_config.ex
@@ -1,19 +1,40 @@
 defmodule Beacon.Sys.SystemConfig do
   use Beacon.Schema
   import Ecto.Changeset
+  alias Beacon.Constant
 
   @schema_prefix "sys"
 
   schema "system_configs" do
+    field :public_id, Ecto.UUID, autogenerate: false, read_after_writes: true
 
+    field :key, :string
+    field :value, :string
+    field :description, :string
 
-    timestamps(type: :utc_datetime)
+    belongs_to :updated_by, Beacon.Iam.Profile
+
+    timestamps()
   end
 
-  @doc false
+  @required_fields [
+    :public_id,
+    :key,
+    :value
+  ]
+  @optional_fields [
+    :description,
+    :updated_by_id
+  ]
   def changeset(system_config, attrs) do
     system_config
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:key, min: 1)
+    |> validate_length(:value, min: 1)
+    |> validate_length(:description, max: Constant.text_max_length())
+    |> foreign_key_constraint(:updated_by_id)
+    |> unique_constraint(:key, name: :system_configs_key_idx)
+    |> unique_constraint(:public_id, name: :system_configs_public_id_idx)
   end
 end
