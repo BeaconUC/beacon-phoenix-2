c4791b192354a79953d8a7412e6d8b91d12bc25e
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2025-12-29 12:41:37 +0000

    refactor: update Scope module to preload user profile and include role in scope struct

--- lib/beacon/accounts/scope.ex
+++ lib/beacon/accounts/scope.ex
@@ -17,16 +17,27 @@ defmodule Beacon.Accounts.Scope do
   """
 
   alias Beacon.Accounts.User
+  alias Beacon.Repo
 
-  defstruct user: nil
+  defstruct user: nil, profile: nil, role: nil
 
   @doc """
   Creates a scope for the given user.
 
   Returns nil if no user is given.
   """
+  # def for_user(%User{} = user) do
+  #   %__MODULE__{user: user}
+  # end
+
   def for_user(%User{} = user) do
-    %__MODULE__{user: user}
+    user = Repo.preload(user, :profile)
+
+    %__MODULE__{
+      user: user,
+      profile: user.profile,
+      role: user.profile && user.profile.role
+    }
   end
 
   def for_user(nil), do: nil
