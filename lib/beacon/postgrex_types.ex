50ed9decb6fc830d5b6c397c30d177cd767e9e0a
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2026-01-04 17:30:01 +0000

    style: format Postgrex.Types.define for improved readability

--- lib/beacon/postgrex_types.ex
+++ lib/beacon/postgrex_types.ex
@@ -1,3 +1,5 @@
-Postgrex.Types.define(Beacon.PostgresTypes,
+Postgrex.Types.define(
+  Beacon.PostgresTypes,
   [Geo.PostGIS.Extension] ++ Ecto.Adapters.Postgres.extensions(),
-  json: Jason)
+  json: Jason
+)
